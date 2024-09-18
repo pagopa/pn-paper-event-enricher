fecf3fa679f984ed62cedbfcc194b432564a8832
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-09-18 13:37:09 +0200

    [PN-12694] Added queue and pipe for pn-paper-archive

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,9 +1,24 @@
-## Quando viene aggiornato questo file, aggiornare anche il commitId presente nel file initsh-for-testcontainer-sh
+echo "### CREATE QUEUES FOR PN-PAPER-EVENT-ENRICHER ###"
+queues="pn-paper-event-enrichment-input pn-paper-archives"
+for qn in $(echo $queues | tr " " "\n");do
+  echo creating queue $qn ...
+  aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+      sqs create-queue \
+      --attributes '{"DelaySeconds":"2"}'\
+      --queue-name $qn
+done
 
-echo "### CREATE PN-TEMPLATE QUEUES ###"
+echo "### CREATE PN PAPER CHANNEL EVENT ENRICHMENT TABLE ###"
 
-echo "### CREATE PN-TEMPLATE TABLES ###"
-
-
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-PaperChannelEventEnrichment \
+    --attribute-definitions \
+        AttributeName=hashKey,AttributeType=S \
+        AttributeName=sortKey,AttributeType=S \
+    --key-schema \
+        AttributeName=hashKey,KeyType=HASH \
+        AttributeName=sortKey,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5
 
 echo "Initialization terminated"
