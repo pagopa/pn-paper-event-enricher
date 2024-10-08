57b471a0844d950ed55dd6436a874dbb7c1072dc
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-10-08 12:46:10 +0200

    clean code smells

--- src/test/java/it/pagopa/pn/paper/event/enricher/middleware/db/Con020ArchiveDaoIT.java
+++ src/test/java/it/pagopa/pn/paper/event/enricher/middleware/db/Con020ArchiveDaoIT.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,13 +24,13 @@ class Con020ArchiveDaoIT extends BaseTest.WithLocalStack {
     private Con020ArchiveDaoImpl con020ArchiveDao;
 
 
-    private static CON020ArchiveEntity createArchiveEntity(String hashKey, String sortKey) {
+    private static CON020ArchiveEntity createArchiveEntity(String hashKey) {
         CON020ArchiveEntity con020ArchiveEntity = new CON020ArchiveEntity();
         con020ArchiveEntity.setHashKey(hashKey);
         con020ArchiveEntity.setArchiveStatus("NEW");
         con020ArchiveEntity.setArchiveFileKey("archiveFileKey");
         con020ArchiveEntity.setEntityName("entityName");
-        con020ArchiveEntity.setSortKey(sortKey);
+        con020ArchiveEntity.setSortKey("sortKey");
         con020ArchiveEntity.setProcessingTask("processingTask");
         con020ArchiveEntity.setLastModificationTime(Instant.now());
         con020ArchiveEntity.setRecordCreationTime(Instant.now());
@@ -42,7 +43,7 @@ class Con020ArchiveDaoIT extends BaseTest.WithLocalStack {
     void putIfAbsentAndUpdateIfExist_testOK() {
         String hashKey = UUID.randomUUID().toString();
 
-        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey, "sortKey");
+        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey);
 
         con020ArchiveDao.putIfAbsent(con020ArchiveEntity).block();
 
@@ -56,7 +57,7 @@ class Con020ArchiveDaoIT extends BaseTest.WithLocalStack {
     void putIfAbsentAndUpdateIfExist_updateKO_itemDoesNotExists() {
         String hashKey = UUID.randomUUID().toString();
 
-        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey, "sortKey");
+        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey);
 
         con020ArchiveDao.putIfAbsent(con020ArchiveEntity).block();
 
@@ -64,8 +65,10 @@ class Con020ArchiveDaoIT extends BaseTest.WithLocalStack {
         con020ArchiveEntity.setHashKey(UUID.randomUUID().toString());
         con020ArchiveEntity.setArchiveFileKey("test_ko");
 
+        Executable executable = () -> con020ArchiveDao.updateIfExists(con020ArchiveEntity).block();
+
         Assertions.assertThrows(PaperEventEnricherException.class,
-                () -> con020ArchiveDao.updateIfExists(con020ArchiveEntity).block(),
+                executable,
                 "Con020ArchiveDao doesn't exist for ArchiveFileKey: [" + con020ArchiveEntity.getArchiveFileKey() + "]");
     }
 
@@ -73,12 +76,14 @@ class Con020ArchiveDaoIT extends BaseTest.WithLocalStack {
     @Test
     void putIfAbsentAndUpdateIfExist_putKO_itemAlreadyExists() {
         String hashKey = UUID.randomUUID().toString();
-        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey, "sortKey");
+        CON020ArchiveEntity con020ArchiveEntity = createArchiveEntity(hashKey);
 
         con020ArchiveDao.putIfAbsent(con020ArchiveEntity).block();
 
+        Executable executable = () -> con020ArchiveDao.putIfAbsent(con020ArchiveEntity).block();
+
         Assertions.assertThrows(PaperEventEnricherException.class,
-                () -> con020ArchiveDao.putIfAbsent(con020ArchiveEntity).block(),
+                executable,
                 "Con020ArchiveDao already exist for ArchiveFileKey: [" + con020ArchiveEntity.getArchiveFileKey() + "]");
     }
 }
