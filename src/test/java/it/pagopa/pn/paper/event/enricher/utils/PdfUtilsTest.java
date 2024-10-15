857fca757a5dff85fee58e538a20683164701082
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-10-15 18:34:02 +0200

    [PN-12914] Refactor PdfUtilsTest using ParameterizedTest

--- src/test/java/it/pagopa/pn/paper/event/enricher/utils/PdfUtilsTest.java
+++ src/test/java/it/pagopa/pn/paper/event/enricher/utils/PdfUtilsTest.java
@@ -5,41 +5,35 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 class PdfUtilsTest {
 
-    @Test
-    void cutPdf1Page() throws IOException {
-        Path path = Paths.get("src/test/resources/attachment_example_1.pdf");
-        byte[] fileContent = Files.readAllBytes(path);
-        byte[] cuttedPdf = PdfUtils.cutPdf(fileContent, 2);
-        try(PDDocument document = Loader.loadPDF(cuttedPdf)){
-            Assertions.assertEquals(1, document.getNumberOfPages());
-        }
+
+    static Stream<Arguments> pdfProvider() {
+        return Stream.of(
+                Arguments.of("src/test/resources/attachment_example_1.pdf", 1),
+                Arguments.of("src/test/resources/attachment_example_2.pdf", 2),
+                Arguments.of("src/test/resources/attachment_example_3.pdf", 2)
+        );
     }
 
-    @Test
-    void cutPdf2Page() throws IOException {
-        Path path = Paths.get("src/test/resources/attachment_example_2.pdf");
+    @ParameterizedTest
+    @MethodSource("pdfProvider")
+    void cutPdf(String filePath, int expectedPages) throws IOException {
+        Path path = Paths.get(filePath);
         byte[] fileContent = Files.readAllBytes(path);
         byte[] cuttedPdf = PdfUtils.cutPdf(fileContent, 2);
-        try(PDDocument document = Loader.loadPDF(cuttedPdf)){
-            Assertions.assertEquals(2, document.getNumberOfPages());
-        }
-    }
-
-    @Test
-    void cutPdf3Page() throws IOException {
-        Path path = Paths.get("src/test/resources/attachment_example_3.pdf");
-        byte[] fileContent = Files.readAllBytes(path);
-        byte[] cuttedPdf = PdfUtils.cutPdf(fileContent, 2);
-        try(PDDocument document = Loader.loadPDF(cuttedPdf)){
-            Assertions.assertEquals(2, document.getNumberOfPages());
+        try (PDDocument document = Loader.loadPDF(cuttedPdf)) {
+            Assertions.assertEquals(expectedPages, document.getNumberOfPages());
         }
     }
 
