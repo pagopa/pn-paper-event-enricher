4290a10e9dd53322e27986f7f09dc18b7de8681c
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-10-21 15:18:48 +0200

    added script for load test

---
+++ src/test/resources/load-test/main.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { computeSHA256, uploadToSafeStorage } = require('./safestorage.js');
+const { createCSVFromBOL, writeToCSV, duplicateCSVRows } = require('./csv.js');
+const { processBatches } = require('./sqs.js');
+
+async function main() {
+    const bolFile = "attachment_example_3pdf.bol"; //TODO: nel test di carico bol-7000.bol
+    const binFile = "attachment_example_completed_3pdf.zip"; //TODO: nel test di carico zip da 2,5gb
+    const inputCSV = "input-test.csv";
+    const outputCSV = "duplicated_" + inputCSV;
+    const fileKeys = [];
+    const fileNumber = 16;
+    const batchSize = 6; //TODO: nel test di carico 2000
+
+    const sha256 = await computeSHA256(binFile);
+    const archiveDetails = await createCSVFromBOL(bolFile);
+    await writeToCSV(archiveDetails, inputCSV);
+    await uploadToSafeStorage(binFile, fileKeys, sha256, fileNumber);
+    await duplicateCSVRows(inputCSV, outputCSV, fileNumber);
+    await processBatches(outputCSV, fileKeys, sha256, batchSize);
+
+    fs.unlinkSync(outputCSV);
+    fs.unlinkSync(inputCSV);
+
+    console.log(`File ${outputCSV} has been deleted.`);
+}
+
+main().catch(console.error);
---
+++ src/test/resources/load-test/safestorage.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const fs = require('fs');
+const crypto = require('crypto');
+
+const SAFE_STORAGE_URL = 'http://localhost:3000/safe-storage/v1/files';
+
+async function computeSHA256(filePath) {
+    const fileBuffer = fs.readFileSync(filePath);
+    const hash = crypto.createHash('sha256').update(fileBuffer).digest();
+    return hash.toString('base64');
+}
+
+async function uploadToSafeStorage(filePath, fileKeys, sha256, fileNumber) {
+    for (let i = 0; i < fileNumber; i++) {
+        console.log("Start appending file key");
+        fileKeys.push(await createFile(filePath, sha256));
+    }
+}
+
+async function createFile(filePath, sha256) {
+    const cxId = "pn-test";
+    const body = {
+        contentType: "application/octet-stream",
+        documentType: "PN_EXTERNAL_LEGAL_FACTS",
+        status: "SAVED"
+    };
+
+    const headers = {
+        "x-pagopa-safestorage-cx-id": cxId,
+        "x-api-key": "",
+        "x-checksum": "SHA-256",
+        "x-checksum-value": sha256
+    };
+
+    console.log("Start call to safe storage");
+    const response = await axios.post(SAFE_STORAGE_URL, body, { headers });
+    const { uploadUrl, secret, key } = response.data;
+
+    const fileBuffer = fs.readFileSync(filePath);
+    const uploadHeaders = {
+        "Content-Type": "application/octet-stream",
+        "x-amz-checksum-sha256": sha256,
+        "x-amz-meta-secret": secret
+    };
+    const uploadResponse = await axios.put(uploadUrl, fileBuffer, { headers: uploadHeaders });
+
+    if (uploadResponse.status === 200) {
+        console.log(`ARCHIVE FILE LOADED ON SAFESTORAGE KEY ${key}`);
+        return "safestorage://${key}"; //FIXME: LEGGI LA CHIAVE
+    } else {
+        console.log(`Error uploading file: ${uploadResponse.data}`);
+    }
+}
+
+module.exports = { computeSHA256, uploadToSafeStorage };
