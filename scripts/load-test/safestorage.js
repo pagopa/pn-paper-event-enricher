9fddf1ebae69366d082ac9e4e3912045182af988
flaminiaScarciofolo <113031535+flaminiaScarciofolo@users.noreply.github.com>
2024-10-28 08:19:23 +0100

    Load test script (#56)
    * added script for load test
    
    * mv load-test-con020.js
    
    * fix return key in safestorage.js
    
    * fix createEnricher update entity mapper, added env for load test script
    
    * fix safestorage.js for large files upload
    
    * feat: enhanced direct memory size
    
    * fix: incorrect java tool option config name
    
    * disable Keep-Alive in safestorage.js
    
    * add erase_db script
    
    * added log for number of batch
    
    * fix const to let
    
    * edit profile name
    
    * add README.md
    
    ---------
    
    Co-authored-by: vincenzo-racca-pa <vincenzo.racca@pagopa.it>
    Co-authored-by: empassaro <emanuele.passaro@emeal.nttdata.com>

---
+++ scripts/load-test/safestorage.js
@@ -0,0 +1,78 @@
+const axios = require('axios');
+const fs = require('fs');
+const crypto = require('crypto');
+
+async function computeSHA256(filePath) {
+    return "ea25rM8bfJ82//SSbDUu/QEuNxUsEROpt8WNBtZ2aYc=";
+    // const fileBuffer = fs.readFileSync(filePath);
+    // const hash = crypto.createHash('sha256').update(fileBuffer).digest();
+    // return hash.toString('base64');
+}
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function uploadToSafeStorage(filePath, fileKeys, sha256, fileNumber, SAFE_STORAGE_URL) {
+    for (let i = 0; i < fileNumber; i++) {
+        console.log("Start appending file key");
+        fileKeys.push(await createFile(filePath, sha256, SAFE_STORAGE_URL));
+        await delay(3000);
+    }
+}
+
+async function createFile(filePath, sha256, SAFE_STORAGE_URL) {
+    const cxId = "pn-test";
+    const body = {
+        contentType: "application/octet-stream",
+        documentType: "PN_EXTERNAL_LEGAL_FACTS",
+        status: "SAVED"
+    };
+
+    const headers = {
+        "x-pagopa-safestorage-cx-id": cxId,
+        "x-api-key": "",
+        "x-checksum": "SHA-256",
+        "x-checksum-value": sha256
+    };
+
+    console.log("Start call to safe storage");
+    const response = await axios.post(SAFE_STORAGE_URL, body, { headers });
+    const { uploadUrl, secret, key } = response.data;
+    // const uploadResponse = await axios.put(uploadUrl, fileBuffer, { headers: uploadHeaders });
+    const uploadResponse = await uploadToS3PresignedUrl(filePath, uploadUrl, sha256, secret);
+
+    if (uploadResponse.status === 200) {
+        console.log(`ARCHIVE FILE LOADED ON SAFESTORAGE KEY ${key}`);
+        return `safestorage://${key}`;
+    } else {
+        console.log(`Error uploading file: ${uploadResponse.data}`);
+    }
+}
+
+async function uploadToS3PresignedUrl(filePath, uploadUrl, sha256, secret) {
+    const fileSize = fs.statSync(filePath).size; // Ottieni la dimensione del file
+    const readStream = fs.createReadStream(filePath); // Legge il file a blocchi
+
+    try {
+        const uploadResponse = await axios.put(uploadUrl, readStream, {
+            headers: {
+                'Content-Length': fileSize, // S3 richiede la lunghezza del file
+                'Content-Type': 'application/octet-stream', // Imposta il tipo di contenuto corretto
+                "x-amz-checksum-sha256": sha256,
+                "x-amz-meta-secret": secret,
+                'Connection': 'close' // Disabilita il Keep-Alive
+            },
+            maxBodyLength: Infinity, // Evita limiti sulla dimensione del corpo della richiesta
+            maxContentLength: Infinity // Evita limiti sulla dimensione del contenuto
+        })
+            .catch(err => console.log(err));
+        console.log('Upload completato con successo:', uploadResponse.status);
+        return uploadResponse;
+    } catch (error) {
+        console.error('Errore durante l\'upload su S3:', error.message);
+        return null;
+    }
+}
+
+module.exports = { computeSHA256, uploadToSafeStorage };
---
+++ scripts/load-test/sqs.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const { SQSClient, SendMessageCommand, GetQueueUrlCommand } = require('@aws-sdk/client-sqs');
+const { fromIni } = require('@aws-sdk/credential-provider-ini');
+
+const QUEUE_NAME = 'pn-paper-event-enrichment-input';
+
+async function processBatches(outputCSV, fileKeys, sha256, batchSize, profile) {
+    const sqsClient = new SQSClient({
+        region: 'eu-south-1',
+        credentials: fromIni({ profile: profile })
+    });
+    const rows = fs.readFileSync(outputCSV, 'utf-8').split('\n').filter(Boolean);
+
+    const commandGetUrl = new GetQueueUrlCommand({ QueueName: QUEUE_NAME });
+    const responseGetUrl = await sqsClient.send(commandGetUrl);
+    queueUrl = responseGetUrl.QueueUrl;
+    let counter = 0;
+
+    for (let i = 0; i < rows.length; i += batchSize) {
+        counter = counter + 1;
+        const batch = rows.slice(i, i + batchSize);
+        for (let j = 0; j < batch.length; j++) {
+            const fileKey = fileKeys[j % fileKeys.length];
+            console.log(`Processing row ${batch[j]}`);
+            await processRow(fileKey, batch[j], sha256, queueUrl, sqsClient);
+        }
+        console.log(`Processed ${batchSize} rows`);
+        console.log("processed " + counter + " batch");
+        console.log("Waiting 1 minute...");
+        await new Promise(resolve => setTimeout(resolve, 300000));
+    }
+}
+
+async function processRow(fileKey, row, sha256, queueUrl, sqsClient) {
+    const [requestId, registeredLetterCode, prodType, dateTime] = parseLine(row);
+    const messageBody = JSON.stringify({
+        analogMail: {
+            requestId,
+            registeredLetterCode,
+            productType: prodType,
+            iun: null,
+            statusCode: "CON020",
+            statusDescription: "Affido conservato",
+            statusDateTime: dateTime,
+            attachments: [{
+                id: "0",
+                documentType: "Affido conservato",
+                uri: fileKey,
+                sha256,
+                date: dateTime
+            }],
+            clientRequestTimeStamp: dateTime
+        },
+        clientId: "pn-cons-000",
+        eventTimestamp: dateTime
+    });
+
+    try {
+        console.log(`Sending message to SQS queue: ${messageBody}`);
+
+        const command = new SendMessageCommand({
+            QueueUrl: queueUrl,
+            MessageBody: messageBody
+            });
+        
+        const response = await sqsClient.send(command);
+        console.log('Message sent:', response.MessageId);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+}
+
+function parseLine(line) {
+    const result = line.split(',');
+    console.log(result);
+    if (result.length < 4) {
+        throw new Error("The line does not contain enough fields.");
+    }
+    return [result[0], result[1], result[2], result[3]];
+}
+
+module.exports = { processBatches };
