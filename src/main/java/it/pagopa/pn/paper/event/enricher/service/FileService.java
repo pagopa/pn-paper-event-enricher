bb5060c56b7db9545cc7269d8e1614dcdb4dc706
Vincenzo Racca <vincenzo.racca@pagopa.it>
2025-01-30 12:09:07 +0100

    PN-13834 - remove null parameter in File.createTempFile

--- src/main/java/it/pagopa/pn/paper/event/enricher/service/FileService.java
+++ src/main/java/it/pagopa/pn/paper/event/enricher/service/FileService.java
@@ -14,7 +14,6 @@ import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipFile;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.stereotype.Component;
@@ -200,7 +199,7 @@ public class FileService {
 
     public Path createTmpFile(String prefix, String suffix) {
         try {
-            return File.createTempFile(TMP_FILE_PREFIX + prefix, suffix, null).toPath();
+            return File.createTempFile(TMP_FILE_PREFIX + prefix, suffix).toPath();
         } catch (IOException e) {
             throw new PaperEventEnricherException(e.getMessage(), 500, UNABLE_TO_CREATE_TMP_FILE);
         }
